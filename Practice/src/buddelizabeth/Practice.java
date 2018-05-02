ac632c6e0e2618918ba3f25122f5625422501fdc
buddelizabeth <budde@EliBudd-PC.localdomain>
2018-05-02 15:45:23 -0500

    Updated stuff

---
+++ Practice/src/buddelizabeth/Practice.java
@@ -0,0 +1,13 @@
+package buddelizabeth;
+
+public class Practice {
+
+    public Practice(String input) {
+        //Something
+    }
+
+    public String getPractice (String something) {
+        //something here.
+        return something;
+    }
+}
---
+++ Practice/src/buddelizabeth/PracticeTest.java
@@ -0,0 +1,18 @@
+package buddelizabeth;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PracticeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPractice() {
+    }
+}
